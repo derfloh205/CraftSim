5119e4acad5d4cf12686ef001c60e9567edae41e
Genju <derfloh205@gmail.com>
2022-11-17 16:35:01 +0100

    base recipe info extracted

--- CraftSim.lua
+++ CraftSim.lua
@@ -1,3 +1,11 @@
+-- TODO: Localization Differences?
+-- TODO: parse from inscription description the skill bonus that is applied upon proc OR calculate with specs in mind
+
+local REAGENT_TYPE = {
+	OPTIONAL = 0,
+	REQUIRED = 1,
+	FINISHING_REAGENT = 2
+}
 
 local addon = CreateFrame("Frame", "CraftSimAddon")
 addon:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
@@ -9,11 +17,18 @@ function addon:ADDON_LOADED(addon_name)
 	if addon_name ~= 'CraftSim' then
 		return
 	end
-	addon:initCraftSimFrame()
 end
 
 function addon:getExportString()
-	local exportString = "HelloWorld"
+	local exportData = addon:exportRecipeData()
+	-- now digest into an export string
+	if exportData == nil then
+		return "CraftSim does only work with Dragonflight profession recipes"
+	end
+	local exportString = ""
+	for property, value in pairs(exportData) do
+		exportString = exportString .. tostring(property) .. "," .. tostring(value) .. "\n"
+	end
 	return exportString
 end
 
@@ -30,9 +45,14 @@ function addon:PLAYER_LOGIN()
 		command = command and command:lower()
 		rest = (rest and rest ~= "") and rest:trim() or nil
 
-		print("CRAFTSIM: Export Data")
-		addon:KethoEditBox_Show(addon:getExportString())
-		KethoEditBoxEditBox:HighlightText()
+		if ProfessionsFrame:IsVisible() and ProfessionsFrame.CraftingPage:IsVisible() then
+			print("CRAFTSIM: Export Data")
+			addon:KethoEditBox_Show(addon:getExportString())
+			KethoEditBoxEditBox:HighlightText()
+		else
+			print("CRAFTSIM ERROR: No Recipe Opened")
+		end
+
 
 	end
 end
@@ -107,4 +127,85 @@ function addon:KethoEditBox_Show(text)
         KethoEditBoxEditBox:SetText(text)
     end
     KethoEditBox:Show()
+end
+
+function addon:getIDFromItemLink(itemLink)
+	return string.find(itemLink, 'item:(%d+)')
+end
+
+function addon:exportRecipeData()
+	local recipeData = {}
+
+	local professionInfo = ProfessionsFrame.professionInfo
+	local professionFullName = professionInfo.professionName
+	local craftingPage = ProfessionsFrame.CraftingPage
+	local schematicForm = craftingPage.SchematicForm
+
+	if not string.find(professionFullName, "Dragon Isles") then
+		return nil
+	end
+
+	recipeData.profession = professionInfo.parentProfessionName
+	local recipeInfo = schematicForm:GetRecipeInfo()
+	local details = schematicForm.Details
+	local operationInfo = details.operationInfo
+	local bonusStats = operationInfo.bonusStats
+
+	local recipeReagents = {}
+	for k, currentSlot in pairs(C_TradeSkillUI.GetRecipeSchematic(recipeInfo.recipeID, false).reagentSlotSchematics) do
+		local reagents = currentSlot.reagents
+		local reagentType = currentSlot.reagentType
+		-- for now only consider the required reagents
+		if reagentType ~= REAGENT_TYPE.REQUIRED then
+			break
+		end
+		recipeReagents[k] = {
+			required = currentSlot.quantityRequired,
+			differentQualities = reagentType == REAGENT_TYPE.REQUIRED and currentSlot.reagents[2] ~= nil,
+			reagentType = currentSlot.reagentType
+		}
+
+		if reagentType == REAGENT_TYPE.REQUIRED and currentSlot.reagents[2] ~= nil then
+			recipeReagents[k].itemsInfo = {}
+			for i, reagent in pairs(reagents) do
+				table.insert(recipeReagents[k].itemsInfo, {
+					itemID = reagent.itemID
+				})
+			end
+		else 
+			recipeReagents[k].itemID = currentSlot.reagents[1].itemID
+		end
+		
+	end
+	for reagentIndex, reagentData in pairs(recipeReagents) do
+		recipeData["reagent" .. reagentIndex .. "Name"] = name
+		recipeData["reagent" .. reagentIndex .. "Required"] = reagentData.required
+		if reagentData.differentQualities then
+			recipeData["reagent" .. reagentIndex .. "q1ID"] = reagentData.itemsInfo[1].itemID
+			recipeData["reagent" .. reagentIndex .. "q2ID"] = reagentData.itemsInfo[2].itemID
+			recipeData["reagent" .. reagentIndex .. "q3ID"] = reagentData.itemsInfo[3].itemID
+		else
+			recipeData["reagent" .. reagentIndex .. "ID"] = itemID
+		end
+	end
+	
+
+	for k, statInfo in pairs(bonusStats) do
+		local statName = statInfo.bonusStatName
+		recipeData[statName .. "Value"] = statInfo.bonusStatValue
+		recipeData[statName .. "Description"] = statInfo.ratingDescription
+		recipeData[statName .. "Percent"] = statInfo.ratingPct
+		if statName == 'Inspiration' then
+			-- matches a row of numbers coming after the % character and any characters in between plus a space, should hopefully match in every localization...
+			local _, _, bonusSkill = string.find(statInfo.ratingDescription, "%%.* (%d+)") 
+			recipeData[statName .. "SkillBonus"] = bonusSkill
+			print("inspirationbonusskill: " .. tostring(bonusSkill))
+		end
+	end
+
+	recipeData["expectedQuality"] = details.craftingQuality
+	recipeData["maxQuality"] = details.maxQuality
+	recipeData["baseItemAmount"] = schematicForm.OutputIcon.Count:GetText()
+
+	return recipeData
 end
