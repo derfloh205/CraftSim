933313bae132a7814df59c74094ab1282584fac1
Bardlock <121692394+Bardlock@users.noreply.github.com>
2022-12-31 13:39:19 +0100

    equip and enchant
    removed the " :" so it can find the strings in the item

--- Locals/frFR.lua
+++ Locals/frFR.lua
@@ -9,8 +9,8 @@ function CraftSim.LOCAL_FR:GetData()
 		[CraftSim.CONST.TEXT.STAT_MULTICRAFT] = "Fabrication multiple",
 		[CraftSim.CONST.TEXT.STAT_RESOURCEFULNESS] = "Ingéniosité",
 		[CraftSim.CONST.TEXT.STAT_CRAFTINGSPEED] = "Vitesse de fabrication",
-		[CraftSim.CONST.TEXT.EQUIP_MATCH_STRING] = "Équipé :",
-		[CraftSim.CONST.TEXT.ENCHANTED_MATCH_STRING] = "Enchanté :",
+		[CraftSim.CONST.TEXT.EQUIP_MATCH_STRING] = "Équipé",
+		[CraftSim.CONST.TEXT.ENCHANTED_MATCH_STRING] = "Enchanté",
 		[CraftSim.CONST.TEXT.INSPIRATIONBONUS_SKILL_ITEM_MATCH_STRING] = "Lors de la fabrication, augmente de",    -- OPTIONAL (Defaulting to EN if not available):
 }
 end
