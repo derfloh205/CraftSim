88eea2d0867bf90de140d10430eb45a481866fbb
Genju <derfloh205@gmail.com>
2023-01-13 14:31:44 +0100

    version up, news update

--- Data/News.lua
+++ Data/News.lua
@@ -22,6 +22,12 @@ function CraftSim.NEWS:GET_NEWS()
     return 
         c("                   Hello and thank you for using CraftSim!\n", bb) .. 
         "             ( Show this window any time with " .. c("/craftsim news", g) .. " )" ..
+        newP("1.8.1") ..
+        P .. c("Jewelcrafting", bb) .. " experimental specialization data added" .. 
+        a .. "This makes knowledge point distribution simulation possible!" .. 
+        a .. "If you want to try it out, activate " .. 
+        a .. c("'Jewelcrafting Experimental Data'", bb) .. 
+        a .. "in the CraftSim Options: " .. c("/craftsim", g) ..
         newP("1.8") ..
         P .. c("Knowledge Distribution Simulation", g) .. " added for " .. 
         a .. c("Blacksmithing", bb) .. " and " .. c("Alchemy", bb) .. " (Experimental Data only)" .. 
@@ -46,68 +52,5 @@ function CraftSim.NEWS:GET_NEWS()
         p .. c("Max Reagent Skill Increase", bb) .. " is now always calculated based " ..
         a .. "on API results." .. 
         a .. "This is because some non recraft recipes are not " ..
-        a .. "using the standard 25% Recipe Difficulty" ..
-        c("\n\n\n--- Version 1.7.6 ---", l) ..
-        c("\n\nSimulation Mode", bb) .. " now correctly considers costs of\noptional and finishing reagents in crafting costs" .. 
-        c("\n\n\n--- Version 1.7.5 ---", l) ..
-        c("\n\n\nMax Reagent Skill Contribution", bb) .. " is now 25% of recipe difficulty\nfor everything except recrafts. For those the\n" ..
-        "factor will be calculated." ..
-        c("\n\n\nMax Reagent Skill %", g) .. " display added in " .. c("Simulation Mode", bb) ..
-        c("\n\n\n--- Version 1.7.4 ---", l) ..
-        "\n\nMany fixes to the optional/finishing reagents in " .. c("Simulation Mode", g) ..
-        "\n\n" .. c("Simulation Mode", g) .. " now correctly shows\nthe OutputItem Links for gear\nconsidering missives, infusions and embellishments" ..
-        c("\n\n\n--- Version 1.7 ---", l) ..
-        "\n\n" .. c("Skill Increase by Reagents", g) .. " is now calculated\nvia an ingame API workaround" ..
-        "\n\n" .. c("Recraft Simulations", bb) .. " are now able to consider\nthe hidden quality modifier" ..
-        c("\n\n\n--- Version 1.6.5 ---", l) ..
-        "\n\n\n" .. "Added Dropdowns for optional and finishing reagents\nin" .. c(" Simulation Mode", g) .. 
-        "\n\nNew Utilities added for" ..
-        "\n" .. c("Debug Mode ( /craftsim debug )", g) ..
-        "\n\nAdded the " .. c("RECrystallize", bb) .. " addon to possible price source addons" ..
-        "\n\nReadjusted the " .. c("Simulation Mode Number Input Width", g) .. "\n to consider numbers with three digits" ..
-        "\n\n" .. c("Cost Overview", g) .. " always show all qualities now" ..
-        "\n\n" .. c("Disabled most Modules for ", r) .. c("Recrafting", bb) .. 
-        "\n" .. " because of wrong reagent skill\ncalculations due to hidden factors" ..
-        "\n\n" .. c("Top Profit", bb) .. " Mode for the Top Gear module is now\navailable for all recipes to support price overrides" ..
-        "\n\n" .. "Added " .. c("CallbackHandler", bb) .. " dependency to loaded list\nPeople who have " .. 
-        c("Ace3", bb) .. " and " .. c("LibCompress", bb) .. " installed manually\nshould now be able to remove them" ..
-        c("\n\nFixed Simulation Mode Help Icons", bb) .. " causing error on hover" ..
-        "\n\nAuto sort quality itemIDs fetched from\nblizzard to prevent wrong quality orders" .. 
-        "\n\n" .. c("Profit Calculation", g) .. " now considers skipping a quality\nwith inspiration" .. 
-        c("\n\n\n--- Version 1.6.4.2 ---", l) ..
-        "\n\n\nFixed TSM Price Expression for Items\nbeing overwritten by the Material one on reload" ..
-        c("\n\n\n--- Version 1.6.4.1 ---", l) ..
-        "\n\n" .. c("Profit Calculation", g) .. " V2 implemented" ..
-        "\n\nThe " .. c("Explanation Window", g) .. "\nshows some small values and an explanation,\nbut is not yet complete!" .. 
-        "\n\n" .. c("Override Crafting Costs Checkbox", g) .. " added" ..
-        "\n\n" .. "Various fixes to " .. c("\nExperimental Data from Profession Specializations", g) .. "\nand now turned off per default" ..
-        c("\n\n\n--- Version 1.6.2, 1.6.3 ---", l) ..
-        "\n\nBugfixes to certain nil errors" ..
-        "\n\nFixed error with saving collapsed status of frames" .. 
-        "\n\nFixed Material Combination Module trying to\nshow up for noQuality recipes" ..
-        "\n\n" .. (tunaData.link or c("Rimefin Tuna", bb)) .. " in cooking recipes\nnow uses " .. (frostedTunaData.link or c("Frosted Rimefin Tuna", bb)) .. " for pricing" ..
-        "\n\n" .. c("Cost Overview", g) .. " now shows the actual item links" ..
-        c("\n\n\n--- Version 1.6.1 ---", l) ..
-        "\n\nVarious fixes to errors popping up for price calculations" .. 
-        c("\n\nWARNING: ", r) .. " Recipes with exactly 1 material that has quality\n" ..
-        "might still suggest wrong low cost max quality combinations!" ..
-        "\n\nThe " .. c("Material Combination Suggestion", r) .. "\nis now disabled for such recipes until this issue is fixed" .. 
-        c("\n\n\n--- Version 1.6 ---", l) ..
-        c("\n\nOverride Sell Price ", g) .. " is a new feature that lets you override\nthe sell price for each quality on an item\n" .. 
-        "even soulbound gear!\nYou can use this to e.g. " .. c("simulate craft order comissions!", g) ..
-        c("\n\nExtra Item Boni", g) .. " for Multicraft and Resourcefulness\nfrom specialization nodes should now be considered correctly!\n\n" ..
-        "New " .. c("Debug Module", g) .. " was added! Are you curious?\nTry " .. c("/craftsim debug\n\n", g) .. 
-        c("WARNING: ", r) .. " Recipes with exactly 1 material that has quality\n" ..
-        "might suggest wrong low cost max quality combinations!" ..
-        "\n\nA new " .. c("Error Window", r) .. " should now pop up.\nIf CraftSim runs into an error" .. 
-        "\n\n--- Version 1.5.7 ---\n\n" ..
-        c("Blacksmithing", bb) .. " and " .. c("Alchemy", bb) .. " now get their stats\n" ..
-        "from your profession spec tree instead from the UI.\n" .. 
-        "This enables some nice new features like a\n" .. c("Specialization" ..
-        " Info Breakdown!", g) .. "\n\nHowever, this is still experimental!\n" ..
-        "If you want, you can opt out in the options ( /craftsim )\n\n" ..
-        "Support for other professions is in progress!\n\n" .. 
-        "What else is new?\n\n" .. 
-        "The " .. c("Simulation Mode", bb) .. " now also includes a modifier for the\n" .. 
-        c("Inspiration Bonus Skill", bb) .. "\nprofession stat"
+        a .. "using the standard 25% Recipe Difficulty"
 end
--- Util/Const.lua
+++ Util/Const.lua
@@ -3,7 +3,7 @@ addonName, CraftSim = ...
 CraftSim.CONST = {}
 
 -- One Time Info ------------
-CraftSim.CONST.currentInfoVersionID = 12 -- last highest: 12
+CraftSim.CONST.currentInfoVersionID = 13 -- last highest: 13
 CraftSim.CONST.debugInfoText = false
 CraftSim.CONST.infoBoxSizeX = 500
 CraftSim.CONST.infoBoxSizeY = 400
