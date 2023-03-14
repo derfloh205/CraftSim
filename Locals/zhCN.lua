75cd4517051f02dca146ec4fa58d481556415e57
ak48disk <ak48disk@users.noreply.github.com>
2023-03-14 12:16:48 -0700

    add zhCN locale

---
+++ Locals/zhCN.lua
@@ -0,0 +1,15 @@
+AddonName, CraftSim = ...
+
+CraftSim.LOCAL_CN = {}
+
+function CraftSim.LOCAL_CN:GetData()
+    return {
+        -- REQUIRED:
+        [CraftSim.CONST.TEXT.STAT_INSPIRATION] = "灵感",
+        [CraftSim.CONST.TEXT.STAT_MULTICRAFT] = "产能",
+        [CraftSim.CONST.TEXT.STAT_RESOURCEFULNESS] = "充裕",
+        [CraftSim.CONST.TEXT.STAT_CRAFTINGSPEED] = "制作速度",
+        [CraftSim.CONST.TEXT.EQUIP_MATCH_STRING] = "装备：",
+        [CraftSim.CONST.TEXT.ENCHANTED_MATCH_STRING] = "附魔：",
+    }
+end
