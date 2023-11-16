233b169d93db1ef5e503ad286177594c309503fe
TheResinger <mororwbenjamin42@gmail.com>
2023-11-16 12:58:13 -0500

    Added 10.2 Optional Reagents

--- Data/OptionalReagentData.lua
+++ Data/OptionalReagentData.lua
@@ -196,12 +196,25 @@ CraftSim.OPTIONAL_REAGENT_DATA = {
     -- Tailoring
     [205012] = {recipeDifficulty = 25}, -- Reserve Parachute
 
+    --- 10.2 Additions
+    -- General
+    [206977] = {recipeDifficulty = 160},-- Enchanted Whelpling's Shadowflame Crest
+    [206960] = {recipeDifficulty = 30}, -- Enchanted Wyrm’s Shadowflame Crest
+    [206961] = {recipeDifficulty = 50}, -- Enchanted Aspect’s Shadowflame Crest
 
+    -- Tailoring
+    [210671] = {recipeDifficulty = 35, qualityID = 1}, -- Verdant Tether
+    [210672] = {recipeDifficulty = 30, qualityID = 2}, 
+    [210673] = {recipeDifficulty = 25, qualityID = 3}, 
 
+    -- Leatherworking
+    [208187] = {recipeDifficulty = 35, qualityID = 1}, -- Verdant Conduit
+    [208188] = {recipeDifficulty = 30, qualityID = 2}, 
+    [208189] = {recipeDifficulty = 25, qualityID = 3}, 
 
-
-
-
-
+    -- Jewelcrafting
+    [208746] = {recipeDifficulty = 35, qualityID = 1}, -- Dreamtender's Charm
+    [208747] = {recipeDifficulty = 30, qualityID = 2}, 
+    [208748] = {recipeDifficulty = 25, qualityID = 3}, 
 
 }
