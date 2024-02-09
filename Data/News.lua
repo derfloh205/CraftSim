9b2c3c9a0335ff4f3fb6daaead80ef5f820f60dd
Genju <derfloh205@gmail.com>
2024-02-09 11:50:29 +0100

    v up

--- Data/News.lua
+++ Data/News.lua
@@ -15,7 +15,7 @@ function CraftSim.NEWS:GET_NEWS(itemMap)
     local news = {
         f.bb("                   Hello and thank you for using CraftSim!\n"),
         f.bb("                                 ( You are awesome! )"),
-        newP("15.0.3"),
+        newP("15.0.4"),
         f.P .. "Introducing " .. f.bb("Sub Recipes Optimization"),
         f.a .. "- " .. f.l("CraftSim") .. " is now able to optimize and cache",
         f.a .. "  crafting cost details for reagents",
