6ebd0c60e6e9f110ad76faebd831b0587f64e7d8
Florian Schneider <derfloh205@gmail.com>
2024-04-15 13:30:28 +0200

    Feature/structural update (#243)
    * pkgmeta
    
    * lib
    
    * use GGUI 2.1

--- Init/GLibs.lua
+++ Init/GLibs.lua
@@ -1,8 +1,8 @@
 ---@class CraftSim
 local CraftSim = select(2, ...)
 
----@type GGUI-2.0
-CraftSim.GGUI = LibStub:GetLibrary("GGUI-2.0")
+---@type GGUI-2.1
+CraftSim.GGUI = LibStub:GetLibrary("GGUI-2.1")
 
 ---@type GUTIL-2.0
 CraftSim.GUTIL = LibStub:GetLibrary("GUTIL-2.0")
