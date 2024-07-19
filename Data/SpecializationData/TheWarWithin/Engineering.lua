effaae127c0359b5c617370ece65cc3137efa867
Genju <derfloh205@gmail.com>
2024-07-19 07:52:04 +0200

    File Bases

---
+++ Data/SpecializationData/TheWarWithin/Engineering.lua
@@ -0,0 +1,7 @@
+---@class CraftSim
+local CraftSim = select(2, ...)
+
+---@type table<string, CraftSim.SPECIALIZATION_DATA.RULE_DATA>
+CraftSim.SPECIALIZATION_DATA.THE_WAR_WITHIN.ENGINEERING_DATA = {
+
+}
