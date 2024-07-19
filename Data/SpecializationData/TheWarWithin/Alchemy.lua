3de510c21d447478f25b0ae579e9c94084dcf41f
Genju <derfloh205@gmail.com>
2024-07-19 15:02:01 +0200

    alchemy prep finished

--- Data/SpecializationData/TheWarWithin/Alchemy.lua
+++ Data/SpecializationData/TheWarWithin/Alchemy.lua
@@ -28,4 +28,54 @@ CraftSim.SPECIALIZATION_DATA.THE_WAR_WITHIN.ALCHEMY_DATA = {
         nodeID = 99015,
         idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
     },
+
+    FANTASTIC_FLASKS_1 = {
+        childNodeIDs = { "BULK_PRODUCTION_FLASKS_1", "PROFESSION_PHIALS_1" },
+        nodeID = 98953,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+    BULK_PRODUCTION_FLASKS_1 = {
+        nodeID = 98952,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+    PROFESSION_PHIALS_1 = {
+        nodeID = 98951,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+
+    POTENT_POTIONS_1 = {
+        childNodeIDs = { "BULK_PRODUCTION_POTIONS_1" },
+        nodeID = 99041,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+    BULK_PRODUCTION_POTIONS_1 = {
+        nodeID = 99040,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+
+    THAUMATURGY_1 = {
+        childNodeIDs = { "MERCURIAL_MATERIALS_1", "VOLATILE_MATERIALS_1", "OMINOUS_MATERIALS_1", "GLEAMING_TRANSMUTAGEN_1", "TRANSMUTATION_1" },
+        nodeID = 99059,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+    MERCURIAL_MATERIALS_1 = {
+        nodeID = 100208,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+    VOLATILE_MATERIALS_1 = {
+        nodeID = 100207,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+    OMINOUS_MATERIALS_1 = {
+        nodeID = 100206,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+    GLEAMING_TRANSMUTAGEN_1 = {
+        nodeID = 100205,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
+    TRANSMUTATION_1 = {
+        nodeID = 99058,
+        idMapping = { [CraftSim.CONST.RECIPE_CATEGORIES.ALL] = {} },
+    },
 }
