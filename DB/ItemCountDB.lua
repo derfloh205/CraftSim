2f2377db2a7f2bc6e44a7c14430b1a937ad1e6d8
Florian Schneider <derfloh205@gmail.com>
2024-07-31 14:22:02 +0200

    consider accountbank for current character in craftqueue (#346)

--- DB/ItemCountDB.lua
+++ DB/ItemCountDB.lua
@@ -8,6 +8,11 @@ local print = CraftSim.DEBUG:SetDebugPrint(CraftSim.CONST.DEBUG_IDS.CACHE_ITEM_C
 ---@class CraftSim.DB
 CraftSim.DB = CraftSim.DB
 
+
+---@class CraftSim.DB.ITEM_COUNT.CharacterItemCountData
+---@field bank table<ItemID, number>
+---@field inventory table<ItemID, number>
+
 ---@class CraftSim.DB.ITEM_COUNT : CraftSim.DB.Repository
 CraftSim.DB.ITEM_COUNT = CraftSim.DB:RegisterRepository()
 
@@ -15,8 +20,12 @@ function CraftSim.DB.ITEM_COUNT:Init()
     if not CraftSimDB.itemCountDB then
         ---@type CraftSimDB.Database
         CraftSimDB.itemCountDB = {
-            ---@type table<string, table<number, number>> table<crafterUID, table<itemID, count>>
-            data = {},
+            data = {
+                ---@type table<ItemID, number>
+                accountBank = {},
+                ---@type table<CrafterUID, CraftSim.DB.ITEM_COUNT.CharacterItemCountData>
+                characters = {}
+            },
             version = 0,
         }
     end
@@ -27,7 +36,6 @@ end
 function CraftSim.DB.ITEM_COUNT:Migrate()
     -- 0 -> 1
     if CraftSimDB.itemCountDB.version == 0 then
-        -- move old saved variable to new db if it exists, otherwise init new table
         CraftSimDB.itemCountDB.data = CraftSimItemCountCache or {}
         CraftSimDB.itemCountDB.version = 1
     end
@@ -42,6 +50,16 @@ function CraftSim.DB.ITEM_COUNT:Migrate()
         end
 
         CraftSimDB.itemCountDB.version = 2
+    end
+
+    -- 2 -> 3 rebuild
+    if CraftSimDB.itemCountDB.version == 2 then
+        self:ClearAll()
+        CraftSimDB.itemCountDB.data = {
+            accountBank = {},
+            characters = {}
+        }
+        CraftSimDB.itemCountDB.version = 3
     end
 end
 
@@ -58,17 +76,68 @@ end
 
 ---@param crafterUID CrafterUID
 ---@param itemID number
+---@param includeBank? boolean -- reagentBank + bank
+---@param includeAccountBank? boolean
 ---@return number itemCount
-function CraftSim.DB.ITEM_COUNT:Get(crafterUID, itemID)
-    CraftSimDB.itemCountDB.data[crafterUID] = CraftSimDB.itemCountDB.data[crafterUID] or {}
-    CraftSimDB.itemCountDB.data[crafterUID][itemID] = CraftSimDB.itemCountDB.data[crafterUID][itemID] or 0
-    return CraftSimDB.itemCountDB.data[crafterUID][itemID]
+function CraftSim.DB.ITEM_COUNT:Get(crafterUID, itemID, includeBank, includeAccountBank)
+    CraftSimDB.itemCountDB.data.characters[crafterUID] = CraftSimDB.itemCountDB.data.characters[crafterUID] or {
+        bank = {},
+        inventory = {}
+    }
+    local itemCount = CraftSimDB.itemCountDB.data.characters[crafterUID].inventory[itemID] or 0
+
+    if includeBank then
+        itemCount = itemCount + (CraftSimDB.itemCountDB.data.characters[crafterUID].bank[itemID] or 0)
+    end
+
+    if includeAccountBank then
+        itemCount = itemCount + self:GetAccountBankCount(itemID)
+    end
+
+    return itemCount
 end
 
 ---@param crafterUID CrafterUID
 ---@param itemID number
 ---@param count number
-function CraftSim.DB.ITEM_COUNT:Save(crafterUID, itemID, count)
-    CraftSimDB.itemCountDB.data[crafterUID] = CraftSimDB.itemCountDB.data[crafterUID] or {}
-    CraftSimDB.itemCountDB.data[crafterUID][itemID] = count
+function CraftSim.DB.ITEM_COUNT:SaveInventoryCount(crafterUID, itemID, count)
+    CraftSimDB.itemCountDB.data.characters[crafterUID] = CraftSimDB.itemCountDB.data.characters[crafterUID] or {
+        bank = {},
+        inventory = {}
+    }
+    CraftSimDB.itemCountDB.data.characters[crafterUID].inventory[itemID] = count
+end
+
+---@param crafterUID CrafterUID
+---@param itemID number
+---@param count number
+function CraftSim.DB.ITEM_COUNT:SaveBankCount(crafterUID, itemID, count)
+    CraftSimDB.itemCountDB.data.characters[crafterUID] = CraftSimDB.itemCountDB.data.characters[crafterUID] or {
+        bank = {},
+        inventory = {}
+    }
+    CraftSimDB.itemCountDB.data.characters[crafterUID].bank[itemID] = count
+end
+
+---@param itemID number
+---@param count number
+function CraftSim.DB.ITEM_COUNT:SaveAccountBankCount(itemID, count)
+    CraftSimDB.itemCountDB.data.accountBank[itemID] = count
+end
+
+---@param crafterUID CrafterUID
+---@param itemID number
+---@param inventory number
+---@param bank number
+---@param accountBank number
+function CraftSim.DB.ITEM_COUNT:UpdateItemCounts(crafterUID, itemID, inventory, bank, accountBank)
+    self:SaveInventoryCount(crafterUID, itemID, inventory)
+    self:SaveBankCount(crafterUID, itemID, bank)
+    self:SaveAccountBankCount(itemID, accountBank)
+end
+
+---@param itemID number
+---@return number count
+function CraftSim.DB.ITEM_COUNT:GetAccountBankCount(itemID)
+    return CraftSimDB.itemCountDB.data.accountBank[itemID] or 0
 end
