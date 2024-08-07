e4c16f9c2f4c2b66d49c6dedbb2d5ba608827fe7
Genju <derfloh205@gmail.com>
2024-08-07 13:21:50 +0200

    script finished

--- DevTools/DataScripts/EnchantData/mapEnchantData.py
+++ DevTools/DataScripts/EnchantData/mapEnchantData.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../')
 import db2Tools
 
-buildVersion = "11.0.2.55059" # Beta 07.08.2024
+buildVersion = "11.0.2.55959" # Beta 07.08.2024
 db2Tables = ["CraftingData", "CraftingDataEnchantQuality"]
 
 if __name__ == '__main__':
@@ -12,15 +12,21 @@ if __name__ == '__main__':
     craftingDataTable = csvTables[0]
     enchantDataTable = csvTables[1]
 
-    enchantDataTable = {}
+    enchantDataMappedTable = {}
 
     for craftingData in craftingDataTable:
         if craftingData["Type"] == "3": # 3 .. Enchant
-            craftingDataID = craftingData["ID"]
+            craftingDataID = int(craftingData["ID"])
+            print(f"Mapping CraftingDataID: {craftingDataID}")
             for enchantData in enchantDataTable:
-                if enchantData["CraftingDataID"] == craftingDataID:
-                    
+                if enchantData["CraftingDataID"] == str(craftingDataID):
+                    qualityID = int(enchantData["Rank"])
+                    itemID = int(enchantData["ItemID"])
+                    if craftingDataID not in enchantDataMappedTable:
+                        enchantDataMappedTable[craftingDataID] = {}
+                    enchantDataMappedTable[craftingDataID][f"q{qualityID}"] = itemID
 
 
-    db2Tools.writeLuaTable(enchantDataTable, "EnchantData", "---@class CraftSim\nlocal CraftSim = select(2, ...)\nCraftSim.ENCHANT_RECIPE_DATA = ", buildVersion)
+
+    db2Tools.writeLuaTable(enchantDataMappedTable, "EnchantData", "---@class CraftSim\nlocal CraftSim = select(2, ...)\nCraftSim.ENCHANT_RECIPE_DATA = ", buildVersion)
         
