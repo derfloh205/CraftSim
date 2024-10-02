21730f7f84a96937704f4bd9e0050897063c9e6d
Tom Cannon <veasu95@gmail.com>
2024-10-02 17:17:46 +0100

    fix: remove comma

--- Modules/CraftQueue/CraftQueue.lua
+++ Modules/CraftQueue/CraftQueue.lua
@@ -818,7 +818,7 @@ function CraftSim.CRAFTQ:AddFirstCrafts()
         local ignoreAcuity = CraftSim.DB.OPTIONS:Get("CRAFTQUEUE_FIRST_CRAFTS_IGNORE_ACUITY_RECIPES")
         local ignoreSpark = CraftSim.DB.OPTIONS:Get("CRAFTQUEUE_FIRST_CRAFTS_IGNORE_SPARK_RECIPES")
         local usesAcuity = recipeData.reagentData:HasOneOfReagents({ CraftSim.CONST.ITEM_IDS.CURRENCY.ARTISANS_ACUITY })
-        local usesSpark = recipeData.reagentData:HasSparkSlot(),
+        local usesSpark = recipeData.reagentData:HasSparkSlot()
         local queueRecipe = isSkillLine and (not ignoreAcuity or not usesAcuity) and (not ignoreSpark or not usesSpark)
         if queueRecipe then
             recipeData.reagentData:SetReagentsMaxByQuality(1)
