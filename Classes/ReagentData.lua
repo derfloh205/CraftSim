6d275d9a83414de23ce3fa75dacd140be3b056f5
Genju <derfloh205@gmail.com>
2024-11-07 09:53:04 +0100

    salvage recipes quantity

--- Classes/ReagentData.lua
+++ Classes/ReagentData.lua
@@ -25,6 +25,11 @@ function CraftSim.ReagentData:new(recipeData, schematicInfo)
     ---@type CraftSim.SalvageReagentSlot
     self.salvageReagentSlot = CraftSim.SalvageReagentSlot(self.recipeData)
 
+    if self.recipeData.isSalvageRecipe then
+        -- https://www.townlong-yak.com/framexml/live/Blizzard_ProfessionsTemplates/Blizzard_ProfessionsRecipeSchematicForm.lua#1136
+        self.salvageReagentSlot.requiredQuantity = schematicInfo.quantityMax
+    end
+
     if not schematicInfo then
         return
     end
