4a9e9c0880ff42708d1645d30844b345a878dbd7
genjuwow <derfloh205@gmail.com>
2025-01-13 06:24:21 +0100

    Add new crafting data for Engineering, Inscription, and Leatherworking specializations (#794)

--- Data/News.lua
+++ Data/News.lua
@@ -17,8 +17,10 @@ function CraftSim.NEWS:GET_NEWS(itemMap)
     local news = {
         f.bb("                   Hello and thank you for using CraftSim!\n"),
         f.bb("                                 ( You are awesome! )"),
+        newP("19.7.4"),
+        f.p .. "11.0.7.58533 Data Update",
         newP("19.7.3"),
-        f.p .. "11.0.7 Data Update",
+        f.p .. "11.0.7.58238 Data Update",
         newP("19.7.2"),
         f.p .. "Fixed " .. f.bb("QuickBuy") .. " reset on list refresh",
         f.a .. "- Now always starts a new search",
