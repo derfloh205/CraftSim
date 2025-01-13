8c5cfa3f88cc7f9606671c0a6c4e887bfb78fe50
Genju <derfloh205@gmail.com>
2025-01-13 07:22:54 +0100

    Bump version to 19.7.5 and update news

--- Data/News.lua
+++ Data/News.lua
@@ -17,6 +17,8 @@ function CraftSim.NEWS:GET_NEWS(itemMap)
     local news = {
         f.bb("                   Hello and thank you for using CraftSim!\n"),
         f.bb("                                 ( You are awesome! )"),
+        newP("19.7.5"),
+        f.p .. "Fixed " .. f.bb("Reagent Optimization") .. " for non quality recipes",
         newP("19.7.4"),
         f.p .. "11.0.7.58533 Data Update",
         newP("19.7.3"),
