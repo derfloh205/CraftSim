7593d07e9a2b23ad73c63236c609f5d984346813
Genju <derfloh205@gmail.com>
2025-10-10 22:27:33 +0200

    news

--- Data/News.lua
+++ Data/News.lua
@@ -21,6 +21,10 @@ function CraftSim.NEWS:GET_NEWS(itemMap)
         CraftSim.MEDIA:GetAsTextIcon(CraftSim.MEDIA.IMAGES.PIXEL_HEART, 0.15)
     local news = {
         f.bb("Hello and thank you for using CraftSim! ( You are awesome! )\n"),
+        newP("20.3.0"),
+        f.s .. "Fixed craft log sometimes not counting crafts",
+        f.a .. "when crafting with low frame rate",
+        collab("https://github.com/shadowmutant"),
         newP("20.2.4"),
         f.p .. "Updated Specialization Data for 11.2.5",
         newP("20.2.3"),
