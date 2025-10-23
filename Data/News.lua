53d051890ea398adc3f15def60695c2ff1fa35cb
Genju <derfloh205@gmail.com>
2025-10-23 20:02:49 +0200

    Specdata Update

--- Data/News.lua
+++ Data/News.lua
@@ -21,6 +21,8 @@ function CraftSim.NEWS:GET_NEWS(itemMap)
         CraftSim.MEDIA:GetAsTextIcon(CraftSim.MEDIA.IMAGES.PIXEL_HEART, 0.15)
     local news = {
         f.bb("Hello and thank you for using CraftSim! ( You are awesome! )\n"),
+        newP("20.3.2"),
+        f.p .. "Updated Specialization Data for 11.2.5.63906",
         newP("20.3.1"),
         f.s .. "Fixed craft log sometimes not counting crafts",
         f.a .. "when crafting with low frame rate",
