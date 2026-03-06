8082a2c7ff343a1ccf8bf7bad9b33ebe34f84e26
Genju <derfloh205@gmail.com>
2026-03-06 19:32:04 +0100

    fix: handle missing currency reward links and update version to 21.2.7

--- Data/News.lua
+++ Data/News.lua
@@ -23,6 +23,9 @@ function CraftSim.NEWS:GET_NEWS(itemMap)
         CraftSim.MEDIA:GetAsTextIcon(CraftSim.MEDIA.IMAGES.PIXEL_HEART, 0.15)
     local news = {
         f.bb("Hello and thank you for using CraftSim! ( You are awesome! )\n"),
+        newP("21.2.7"),
+        f.pg .. f.bb("Craft Log"),
+        f.a .. "- Fixed lua error based on missing currency reward links",
         newP("21.2.6"),
         f.pg .. f.bb("CraftQueue"),
         f.a .. "- Fixed a lua error when queueing partron orders",
