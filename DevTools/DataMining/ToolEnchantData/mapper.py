2e53a5f56eb88c677532671662ffe95c448a4335
Genju <derfloh205@gmail.com>
2026-03-14 12:47:26 +0100

    feat: Enhance ToolEnchantData mapping and update data scripts
    - Added new effect IDs for resourcefulness, multicraft, and ingenuity in mapper.py.
    - Introduced a scaling constant for converting Wago's EffectScalingPoints to actual stat values.
    - Updated the mapping logic to include filtering for specific enchant effects and calculating their values based on item levels.
    - Modified updateAll.py to set debug build and module for ToolEnchantData.
    - Reordered dataScripts to prioritize ToolEnchantData in updateAll.py.
    - Improved searchTable function in wagoTools.py to handle list comparisons for search conditions.

--- DevTools/DataMining/ToolEnchantData/mapper.py
+++ DevTools/DataMining/ToolEnchantData/mapper.py
@@ -3,7 +3,16 @@ sys.path.append('../')
 import wagoTools
 import shutil
 
-wagoTables = ["SpellItemEnchantment", "ItemBonus"]
+EFFECT_IDS = {
+    "resourcefulness": 76,
+    "multicraft": 81,
+    "ingenuity": 82,
+}
+
+# Scaling constant to convert from Wago's EffectScalingPoints to the actual stat value. Calculated based on known values for certain enchants for midnight ilvl ranges.
+scalingConstant = 15 / 10.5783996582
+
+wagoTables = ["SpellItemEnchantment"]
 
 def copy(buildVersion):
     shutil.copy(f"_Result/{buildVersion}/ToolEnchantData.lua", "../../Data/ToolEnchantData.lua")
@@ -11,13 +20,38 @@ def copy(buildVersion):
 def map(buildVersion):
     csvTables = wagoTools.getWagoTables(wagoTables, False, buildVersion)
     spellItemEnchantmentTable = csvTables[0]
-    itemBonusTable = csvTables[1]
 
     toolEnchantData = {}
 
-    for item in spellItemEnchantmentTable:
-        enchantID = item["ID"]
-        
-        toolEnchantData[int(enchantID)] = {"enchantID": enchantID}
+    toolEnchants = wagoTools.searchTable(spellItemEnchantmentTable, 
+                                         {
+                                            "conditions": 
+                                            {
+                                                "EffectArg_0": [str(EFFECT_IDS["resourcefulness"]), str(EFFECT_IDS["multicraft"]), str(EFFECT_IDS["ingenuity"])]
+                                            }
+                                        })
+
+
+    for toolEnchant in toolEnchants:
+        enchantID = int(toolEnchant["ID"])
+        stat = ""
+        if toolEnchant["EffectArg_0"] == str(EFFECT_IDS["resourcefulness"]):
+            stat = "resourcefulness"
+        elif toolEnchant["EffectArg_0"] == str(EFFECT_IDS["multicraft"]):
+            stat = "multicraft"
+        elif toolEnchant["EffectArg_0"] == str(EFFECT_IDS["ingenuity"]):
+            stat = "ingenuity"
+
+        effectScalingPoints = float(toolEnchant["EffectScalingPoints_0"])
+
+        value = round(effectScalingPoints * scalingConstant)
+
+        itemLevelMin = int(toolEnchant["ItemLevelMin"])
+        itemlevelMax = int(toolEnchant["ItemLevelMax"])
+
+        # remove from string suffix beginning with | and trim string
+        name = toolEnchant["Name_lang"].split("|")[0].strip()
+
+        toolEnchantData[int(enchantID)] = {"name": name, "enchantID": enchantID, "stat": stat, "scalingPoints": effectScalingPoints, "itemLevelMin": itemLevelMin, "itemLevelMax": itemlevelMax, "value": value}
 
     wagoTools.writeLuaTable(toolEnchantData, "ToolEnchantData", "---@class CraftSim\nlocal CraftSim = select(2, ...)\nCraftSim.TOOLENCHANTDATA = ", buildVersion)
